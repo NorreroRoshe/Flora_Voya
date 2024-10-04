f121e7751255b2c3fad167b0e53d750461fea882
NorreroRoshe <norikas95@mail.ru>
2024-10-04 19:11:06 +0300

    aqw2wы

--- src/pages/orderConfirmation.jsx
+++ src/pages/orderConfirmation.jsx
@@ -99,9 +99,9 @@
 //                   <div className="service__items-7 animation_service_7" ref={orderRef}> {/* Добавляем ref */}
 //                     <div className="service__item-7">
 //                       <p>
-//                         <h3 className="service__title-7">
+//                         <p className="service__title-7">
 //                           Заказ успешно оформлен!
-//                         </h3>
+//                         </p>
 //                       </p>
 //                       <div>
 //                         Всю информацию мы выслали вам на почту.
@@ -121,9 +121,9 @@
 
 //                     <div className="service__item-7">
 //                       <p>
-//                         <h3 className="service__title-7">
+//                         <p className="service__title-7">
 //                           Информация:
-//                         </h3>
+//                         </p>
 //                       </p>
 //                       <div>
 //                         <p>Имя: {orderData?.name}</p>
@@ -133,9 +133,9 @@
 //                     </div>
 //                     <div className="service__item-7">
 //                       <p>
-//                         <h3 className="service__title-7">
+//                         <p className="service__title-7">
 //                           Доставка:
-//                         </h3>
+//                         </p>
 //                       </p>
 //                       <div>
 //                         <p>Адрес: {orderData?.message}</p>
@@ -144,9 +144,9 @@
 //                     </div>
 //                     <div className="service__item-7">
 //                       <p>
-//                         <h3 className="service__title-7">
+//                         <p className="service__title-7">
 //                           Ваши товары:
-//                         </h3>
+//                         </p>
 //                       </p>
 //                       <ul>
 //                         {cartItems.map((item, index) => (
@@ -160,9 +160,9 @@
 //                     </div>
 //                     <div className="service__item-7">
 //                       <p>
-//                         <h3 className="service__title-7">
+//                         <p className="service__title-7">
 //                           Итог:
-//                         </h3>
+//                         </p>
 //                       </p>
 //                       <div>
 //                         <p>Сумма: {totalPrice} ₽</p>
@@ -171,9 +171,9 @@
 //                     </div>
 //                     <div className="service__item-7">
 //                       <p>
-//                         <h3 className="service__title-7">
+//                         <p className="service__title-7">
 //                           Комментарий:
-//                         </h3>
+//                         </p>
 //                       </p>
 //                       <div>
 //                         {orderData?.company ? (
@@ -200,12 +200,15 @@
 
 // export default OrderConfirmation;
 
+
+
+
 import { useEffect, useState, useRef } from 'react';
 import { useRouter } from 'next/router';
 import Head from "next/head";
 import Link from "next/link";
 import ProductLayout from "@/components/common/layout/ProductLayout";
-import html2pdf from 'html2pdf.js'; // Импортируем библиотеку напрямую
+// import html2pdf from "html2pdf.js";
 
 const OrderConfirmation = () => {
   const router = useRouter();
@@ -228,52 +231,15 @@ const OrderConfirmation = () => {
       setCartItems(JSON.parse(savedCartData));
       setTotalPrice(savedTotalPrice);
       setTotalCount(savedTotalCount);
+    } else {
+    
     }
   }, [router]);
-
-  const downloadOrderAsPDF = () => {
-    const element = orderRef.current;
-    const downloadButton = downloadButtonRef.current;
-    const screenWidth = window.innerWidth;
-
-    let pdfWidth, scale;
-    if (screenWidth >= 1000) {
-      pdfWidth = 1980;
-      scale = 2.5;
-    } else {
-      pdfWidth = 450;
-      scale = 1.5;
-    }
-
-    downloadButton.style.display = 'none';
-    element.style.marginLeft = '55px';
-    element.style.marginTop = '55px';
-
-    const options = {
-      margin: 0.5,
-      filename: 'order-confirmation.pdf',
-      html2canvas: {
-        scale: scale,
-        useCORS: true,
-      },
-      jsPDF: {
-        unit: 'px',
-        format: [pdfWidth, element.clientHeight * scale],
-        orientation: 'portrait',
-      },
-    };
-
-    html2pdf().from(element).set(options).save().then(() => {
-      downloadButton.style.display = 'block';
-      element.style.marginLeft = '0';
-      element.style.marginTop = '0';
-    });
-  };
 
   if (!orderData) {
     return <p>Нет данных о заказе. Пожалуйста, вернитесь на главную.</p>;
   }
-
+  
   return (
     <>
       <Head>
@@ -287,24 +253,34 @@ const OrderConfirmation = () => {
             <div className="container">
               <div className="row">
                 <div className="col-xxl-12">
-                  <div className="service__items-7 animation_service_7" ref={orderRef}>
+                  <div className="service__items-7 animation_service_7" ref={orderRef}> {/* Добавляем ref */}
                     <div className="service__item-7">
-                      <h3 className="service__title-7">Заказ успешно оформлен!</h3>
-                      <div>Всю информацию мы выслали вам на почту.</div>
+                      <p>
+                        <p className="service__title-7">
+                          Заказ успешно оформлен!
+                        </p>
+                      </p>
+                      <div>
+                        Всю информацию мы выслали вам на почту.
+                      </div>
                       <button
-                        onClick={downloadOrderAsPDF}
                         className="wc-btn-dark wc-btn-dark1"
                         style={{ marginTop: '20px' }}
                         ref={downloadButtonRef}
                       >
                         Скачать лист заказа (.pdf)
                       </button>
-                      <p style={{ marginTop: '10px', fontSize: '11px' }}>
-                        * Рекомендуем скачать лист заказа, так как данные по заказу удалятся с сайта. В любом случае всю информацию мы вышлем вам на почту или мессенджеры.
+                      <p style={{marginTop: '10px', fontSize: '11px'}}>
+                       * Рекомендуем скачать лист заказа, так как данные по заказу удаляться с сайта. В любом случае всю информацтю мы вышлем вам на почту или мессенджеры.
                       </p>
                     </div>
+
                     <div className="service__item-7">
-                      <h3 className="service__title-7">Информация:</h3>
+                      <p>
+                        <p className="service__title-7">
+                          Информация:
+                        </p>
+                      </p>
                       <div>
                         <p>Имя: {orderData?.name}</p>
                         <p>Телефон: {orderData?.phone}</p>
@@ -312,18 +288,26 @@ const OrderConfirmation = () => {
                       </div>
                     </div>
                     <div className="service__item-7">
-                      <h3 className="service__title-7">Доставка:</h3>
+                      <p>
+                        <p className="service__title-7">
+                          Доставка:
+                        </p>
+                      </p>
                       <div>
                         <p>Адрес: {orderData?.message}</p>
                         <p>Дата и время: {orderData?.phone}</p>
                       </div>
                     </div>
                     <div className="service__item-7">
-                      <h3 className="service__title-7">Ваши товары:</h3>
+                      <p>
+                        <p className="service__title-7">
+                          Ваши товары:
+                        </p>
+                      </p>
                       <ul>
                         {cartItems.map((item, index) => (
                           <li key={index}>
-                            <Link href={`/shop/${item.id}`} style={{ color: '#000', display: 'block' }}>
+                            <Link href={`/shop/${item.id}`} style={{color: '#000', display: 'block'}}>
                               ● {item.title} - {item.quantity} шт. - {item.price} ₽
                             </Link>
                           </li>
@@ -331,19 +315,31 @@ const OrderConfirmation = () => {
                       </ul>
                     </div>
                     <div className="service__item-7">
-                      <h3 className="service__title-7">Итог:</h3>
+                      <p>
+                        <p className="service__title-7">
+                          Итог:
+                        </p>
+                      </p>
                       <div>
                         <p>Сумма: {totalPrice} ₽</p>
                         <p>Позиций: {totalCount}</p>
                       </div>
                     </div>
                     <div className="service__item-7">
-                      <h3 className="service__title-7">Комментарий:</h3>
+                      <p>
+                        <p className="service__title-7">
+                          Комментарий:
+                        </p>
+                      </p>
                       <div>
                         {orderData?.company ? (
-                          <>{orderData?.company}</>
+                          <>
+                            {orderData?.company}
+                          </>
                         ) : (
-                          <p style={{ color: 'red' }}>Без комментариев</p>
+                          <p style={{color: 'red'}}>
+                            Без комментариев
+                          </p>
                         )}
                       </div>
                     </div>
