95d6eb9be52c93553911c3b450f74e90e38d05f9
NorreroRoshe <norikas95@mail.ru>
2024-10-04 19:47:50 +0300

    aqw2wы

--- src/pages/orderConfirmation.jsx
+++ src/pages/orderConfirmation.jsx
@@ -201,14 +201,13 @@
 // export default OrderConfirmation;
 
 
-
-
 import { useEffect, useState, useRef } from 'react';
 import { useRouter } from 'next/router';
 import Head from "next/head";
 import Link from "next/link";
 import ProductLayout from "@/components/common/layout/ProductLayout";
-// import html2pdf from "html2pdf.js";
+import { jsPDF } from "jspdf";
+import html2canvas from "html2canvas";
 
 const OrderConfirmation = () => {
   const router = useRouter();
@@ -231,15 +230,49 @@ const OrderConfirmation = () => {
       setCartItems(JSON.parse(savedCartData));
       setTotalPrice(savedTotalPrice);
       setTotalCount(savedTotalCount);
-    } else {
-    
-    }
+    } 
   }, [router]);
+
+
+  const downloadPDF = () => {
+    // Hide the download button
+    if (downloadButtonRef.current) {
+      downloadButtonRef.current.style.display = 'none';
+    }
+
+    html2canvas(orderRef.current).then(canvas => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF();
+      const imgWidth = 190; // Set the image width
+      const pageHeight = pdf.internal.pageSize.height;
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+      let heightLeft = imgHeight;
+      let position = 55; // Add a top margin of 55px
+
+      // Add the image to the PDF with the top margin
+      pdf.addImage(imgData, 'PNG', 10, position, imgWidth, imgHeight);
+      heightLeft -= pageHeight;
+
+      while (heightLeft >= 0) {
+        position = heightLeft - imgHeight;
+        pdf.addPage();
+        pdf.addImage(imgData, 'PNG', 10, position, imgWidth, imgHeight);
+        heightLeft -= pageHeight;
+      }
+
+      pdf.save('order_confirmation.pdf');
+
+      // Show the download button again
+      if (downloadButtonRef.current) {
+        downloadButtonRef.current.style.display = 'block';
+      }
+    });
+  };
 
   if (!orderData) {
     return <p>Нет данных о заказе. Пожалуйста, вернитесь на главную.</p>;
   }
-  
+
   return (
     <>
       <Head>
@@ -253,34 +286,25 @@ const OrderConfirmation = () => {
             <div className="container">
               <div className="row">
                 <div className="col-xxl-12">
-                  <div className="service__items-7 animation_service_7" ref={orderRef}> {/* Добавляем ref */}
+                  <div className="service__items-7 animation_service_7" ref={orderRef}>
                     <div className="service__item-7">
-                      <p>
-                        <p className="service__title-7">
-                          Заказ успешно оформлен!
-                        </p>
-                      </p>
-                      <div>
-                        Всю информацию мы выслали вам на почту.
-                      </div>
+                      <p className="service__title-7">Заказ успешно оформлен!</p>
+                      <div>Всю информацию мы выслали вам на почту.</div>
                       <button
                         className="wc-btn-dark wc-btn-dark1"
                         style={{ marginTop: '20px' }}
                         ref={downloadButtonRef}
+                        onClick={downloadPDF} // Add the click handler
                       >
                         Скачать лист заказа (.pdf)
                       </button>
-                      <p style={{marginTop: '10px', fontSize: '11px'}}>
-                       * Рекомендуем скачать лист заказа, так как данные по заказу удаляться с сайта. В любом случае всю информацтю мы вышлем вам на почту или мессенджеры.
+                      <p style={{ marginTop: '10px', fontSize: '11px' }}>
+                        * Рекомендуем скачать лист заказа, так как данные по заказу удаляться с сайта. В любом случае всю информацтю мы вышлем вам на почту или мессенджеры.
                       </p>
                     </div>
 
                     <div className="service__item-7">
-                      <p>
-                        <p className="service__title-7">
-                          Информация:
-                        </p>
-                      </p>
+                      <p className="service__title-7">Информация:</p>
                       <div>
                         <p>Имя: {orderData?.name}</p>
                         <p>Телефон: {orderData?.phone}</p>
@@ -288,26 +312,18 @@ const OrderConfirmation = () => {
                       </div>
                     </div>
                     <div className="service__item-7">
-                      <p>
-                        <p className="service__title-7">
-                          Доставка:
-                        </p>
-                      </p>
+                      <p className="service__title-7">Доставка:</p>
                       <div>
                         <p>Адрес: {orderData?.message}</p>
                         <p>Дата и время: {orderData?.phone}</p>
                       </div>
                     </div>
                     <div className="service__item-7">
-                      <p>
-                        <p className="service__title-7">
-                          Ваши товары:
-                        </p>
-                      </p>
+                      <p className="service__title-7">Ваши товары:</p>
                       <ul>
                         {cartItems.map((item, index) => (
                           <li key={index}>
-                            <Link href={`/shop/${item.id}`} style={{color: '#000', display: 'block'}}>
+                            <Link href={`/shop/${item.id}`} style={{ color: '#000', display: 'block' }}>
                               ● {item.title} - {item.quantity} шт. - {item.price} ₽
                             </Link>
                           </li>
@@ -315,31 +331,19 @@ const OrderConfirmation = () => {
                       </ul>
                     </div>
                     <div className="service__item-7">
-                      <p>
-                        <p className="service__title-7">
-                          Итог:
-                        </p>
-                      </p>
+                      <p className="service__title-7">Итог:</p>
                       <div>
                         <p>Сумма: {totalPrice} ₽</p>
                         <p>Позиций: {totalCount}</p>
                       </div>
                     </div>
                     <div className="service__item-7">
-                      <p>
-                        <p className="service__title-7">
-                          Комментарий:
-                        </p>
-                      </p>
+                      <p className="service__title-7">Комментарий:</p>
                       <div>
                         {orderData?.company ? (
-                          <>
-                            {orderData?.company}
-                          </>
+                          <>{orderData?.company}</>
                         ) : (
-                          <p style={{color: 'red'}}>
-                            Без комментариев
-                          </p>
+                          <p style={{ color: 'red' }}>Без комментариев</p>
                         )}
                       </div>
                     </div>
